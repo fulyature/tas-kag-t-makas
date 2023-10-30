93bf176297390868ffc1e3ce210dfc41f9070931
fulya_ture <fulyature@gmail.com>
2023-10-30 11:06:57 +0300

    selectors

---
+++ app.js
@@ -0,0 +1,17 @@
+//**********SELECTORS***************
+
+//resimler
+const rockImage = document.getElementById("rock");
+const paperImage = document.getElementById("paper");
+
+const scissorImage = document.getElementById("scissor");
+
+//secılen elemanların tasıyıcıları
+const yourChoiceDiv = document.getElementById("your-choice");
+//div in içinde element oldu. anlamak ıcın bu sekıl ısımlendırdık
+
+//**********VARİABLES***************
+
+//**********EVENT LİSTENERS**********
+
+//**********FUNCTİONS***************
