4906ce582276ecdd1b5cb66c5d59625be242edeb
fulya_ture <fulyature@gmail.com>
2023-10-30 19:34:26 +0300

    draw

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const selectionArticle = document.querySelector(".selection");
 
 // //resimler
+// //----ilkel yontem
 // const rockImage = document.getElementById("rock");
 // const paperImage = document.getElementById("paper");
 
@@ -14,9 +15,21 @@ const yourChoiceDiv = document.getElementById("your-choice");
 const pcChoiceDiv = document.getElementById("pc-choice");
 //div in içinde element oldu. anlamak ıcın bu sekıl ısımlendırdık
 
+//message
+const messagePar = document.querySelector(".message");
+
+//scorecard
+const scoreCardSection = document.querySelector(".score-card");
+
 //**********VARİABLES***************
 let userSelectImg = document.createElement("img");
 let pcSelectImg = document.createElement("img");
+
+//colors
+const YELLOW = "#ffc538";
+const RED = "#f5415c";
+const GREEN = "#5ab781";
+
 //**********EVENT LİSTENERS**********
 selectionArticle.addEventListener("click", (e) => {
   // console.log(e.target.id)
@@ -34,8 +47,22 @@ const createPcSelection = () => {
   pcSelectImg.src = `./assets/${pcRandom}.png`;
   pcSelectImg.alt = pcRandom;
   pcChoiceDiv.appendChild(pcSelectImg);
+  calculateResult();
 };
 
+const calculateResult = () => {
+  //   console.log(userSelectImg);
+  //eşitlik;
+  if (userSelectImg.alt === pcSelectImg.alt) {
+    draw();
+  }
+};
+
+const draw = () => {
+  messagePar.textContent = "its a draw";
+  scoreCardSection.style.color = YELLOW;
+  messagePar.style.backgroundColor = YELLOW;
+};
 // rockImage.addEventListener("click", () => {
 //   yourChoiceDiv.innerHTML = `<img src="./assets/rock.png" alt="rock">`;
 // });
